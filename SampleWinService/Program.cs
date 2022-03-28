792d2c3008d8cd8a50e1ff5fc9dcb2afb4f61c7a
00114451 <Tsuyoshi.K@yokogawa.com>
2022-03-28 19:00:50 +0900

    初期コミット-プロジェクトの移動

---
+++ SampleWinService/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceProcess;
+using System.Text;
+
+namespace SampleWinService
+{
+    static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[] 
+			{ 
+				new Service1() 
+			};
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
---
+++ SampleWinService/Service1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.ServiceProcess;
+using System.Text;
+
+namespace SampleWinService
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            base.OnStart(args);
+            /* ここにサービス開始時の処理を記述する */
+        }
+
+        protected override void OnStop()
+        {
+            base.OnStop();
+            /* ここにサービス停止時の処理を記述する */
+        }
+    }
+}
